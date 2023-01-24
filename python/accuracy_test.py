36e7609eec2fbececa07904609962fa52ba1e79a
flynn <fulin@uw.edu>
2023-01-23 23:11:43 -0800

    add ill condition test result

--- python/accuracy_test.py
+++ python/accuracy_test.py
@@ -2,7 +2,6 @@ import numpy as np
 from numpy import linalg
 from qr import qr
 from utils import get_error, generate_matrix
-from prettytable import PrettyTable
 
 def test():
     tests = [
@@ -63,27 +62,5 @@ def edge_case_test():
         expectError = 1e-8
         assert error < expectError, f"error is {error}, expect small than {expectError}"
 
-def ill_conditioned_test():
-    sizes = [10, 100]
-    condition_numbers = [3,4,5,6,7]
-    
-    rows = []
-    for n in sizes:
-        for con in condition_numbers:
-            A = generate_matrix(n, 10 ** con)
-
-            Q, R = qr(A)
-            Q1, R1 = qr(A, dtype=np.float32)
-            _Q,_R = linalg.qr(A)
-
-            rows.append(
-                [(n,f'10^{con}'), get_error(A,Q,R), get_error(A,Q1,R1), get_error(A,_Q,_R)]
-            )
-    table = PrettyTable()
-    table.field_names = ["(n, condition_num)", "qr float64", "qr float32", "numpy(lapack) qr float64"]
-    table.add_rows(rows)
-    print(table)
-
 test()
 edge_case_test()
-ill_conditioned_test()
