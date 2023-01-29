8fd8b84b359341d50cde06be4e8b3e9f8c056915
flynn <fulin@uw.edu>
2023-01-28 19:23:28 -0800

    test in float16

---
+++ python/linear_least_sqare.py
@@ -0,0 +1,4 @@
+from utils import get_error, generate_matrix
+import numpy as np
+A = generate_matrix(10, 10)
+b = np.random.rand(10)
--- python/performance_test.py
+++ python/performance_test.py
@@ -1,19 +1,53 @@
 import numpy as np
 from numpy import linalg
+from qr import householder_qr
+from utils import get_error, generate_matrix
+from prettytable import PrettyTable, MARKDOWN
 import time
-from qr import qr
+import os
 
-def test_perfomance():
-    for dtype in np.float64, np.float32:
-        for n in 100,1000:
-            A = np.random.random((n, n)).astype(dtype)
-            start_time = time.time()
-            Q, R = qr(A, dtype=dtype)
-            print(f'{n}x{n} {dtype} matrix cost time: {time.time() - start_time} seconds')
+def get_error_and_duration(A, qr_fn):
+    start_time = time.time()
+    Q, R = qr_fn(A)
+    end_time = time.time()
+    return format(get_error(A,Q,R), '.2e'), round(end_time - start_time, 2)
 
-            A = np.random.random((n, n)).astype(dtype)
-            start_time = time.time()
-            Q, R = linalg.qr(A, mode='complete')
-            print(f'{n}x{n} {dtype} matrix numpy cost time: {time.time() - start_time} seconds')
+def qr_float32(A):
+    return householder_qr(A, dtype=np.float32)
 
-test_perfomance()
+def qr_float16(A):
+    return householder_qr(A, dtype=np.float16)
+
+def test():
+    sizes = [10, 100, 1000]
+    condition_numbers = [3,4,5,6,7]
+    
+    duration_rows = []
+    error_rows = []
+    for n in sizes:
+        for con in condition_numbers:
+            A = generate_matrix(n, 10 ** con)
+            duration_row, error_row = [(n,f'10^{con}')], [(n,f'10^{con}')]
+
+            for fn in qr_float16,qr_float32,householder_qr, linalg.qr:
+                error, duration = get_error_and_duration(A, fn)
+                duration_row.append(f'{duration}s')
+                error_row.append(error)
+
+            duration_rows.append(duration_row)
+            error_rows.append(error_row)
+
+    error_table, duration_table = PrettyTable(), PrettyTable()
+    error_table.set_style(MARKDOWN)
+    duration_table.set_style(MARKDOWN)
+    error_table.field_names = ["(n, condition_num)", "qr float16", "qr float32", "qr float64", "numpy(lapack) qr float64"]
+    duration_table.field_names = ["(n, condition_num)", "qr float16", "qr float32", "qr float64", "numpy(lapack) qr float64"]
+    error_table.add_rows(error_rows)
+    duration_table.add_rows(duration_rows)
+    with open(os.path.join(os.path.dirname(__file__), './performanc e_test_result/error.md'), 'w') as f:
+        f.write(error_table.get_string())
+    with open(os.path.join(os.path.dirname(__file__), './performance_test_result/duration.md'), 'w') as f:
+        f.write(duration_table.get_string())
+
+test()
+
