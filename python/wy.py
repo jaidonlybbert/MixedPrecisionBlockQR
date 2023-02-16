e7311ba316afc9820a9066c660c5fac89eb02e23
flynn <fulin@uw.edu>
2023-02-16 14:24:47 -0800

    simplify code of WY

--- python/wy.py
+++ python/wy.py
@@ -15,17 +15,14 @@ def wy_representation(V, B) -> np.ndarray:
         B (np.ndarray): matrix B containing n coefficients [beta0, beta1, ..., betan] derived from factored forms 
     """
 
-    m = len(V[0])
-    r = len(V)
-
-    Y = np.array(V[0]).reshape(m, 1)
-    W = np.array(B[0]*V[0]).reshape(m, 1)
+    m,r = len(V[0]),len(V)
+    Y = np.array(V[0]).reshape(-1, 1)
+    W = np.array(B[0]*V[0]).reshape(-1, 1)
 
     for i in range(1, r):
-        z = B[i] * np.dot((np.identity(m) - np.matmul(W, np.transpose(Y))), V[i])
-        z = z.reshape(m, 1)
-        W = np.concatenate((W, z), axis=1)
-        Y = np.concatenate((Y, V[i].reshape(m, 1)), axis=1)
+        z = B[i] * (np.identity(m) - W.dot(Y.T)).dot(V[i])
+        W = np.c_[W,z]
+        Y = np.c_[Y,V[i]]
     
     return W, Y
 
