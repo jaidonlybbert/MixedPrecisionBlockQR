0489f27c730b2b20696e2991946dc39ed1e9fe41
jaidonlybbert <jaidonlybbert@gmail.com>
2023-02-27 17:44:11 -0800

    Updated CAQR

--- python/ts_qr.py
+++ python/ts_qr.py
@@ -8,14 +8,12 @@ def arrange_to_diag(A,B):
     down_left = np.zeros((m2, n1))
     top_right = np.zeros((m1, n2))
     return np.c_[np.r_[A,down_left], np.r_[top_right, B]]
-
 def arrange_matrixs_to_diag(*matrixs):
     current, i, n = matrixs[0],1,len(matrixs)
     while i < n:
        current = arrange_to_diag(current, matrixs[i])
        i += 1
     return current
-
 
 def ts_qr(A):
     m, n = A.shape
@@ -34,19 +32,44 @@ def ts_qr(A):
     L1 = arrange_matrixs_to_diag(U1, U2, U3, U4) 
     L2 = arrange_matrixs_to_diag(U12, U34)
     Q = L1.dot(L2).dot(U1234)
-    return Q,R1234
-
-
+    reduction_tree = [U1234, U12, U34, U1,U2,U3,U4]
+    return Q,R1234, reduction_tree
     
-# def tiled_qr(A):
-#     B1 = A[:,0:3]
-#     return ts_qr(B1)
+def tiled_qr(A):
+    B1 = A[:,0:3]
+    Q1,R1, reduction_tree = ts_qr(B1)
+    U1234, U12, U34, U1,U2,U3,U4 = reduction_tree
+    # Apply QT horizontally across trailing matrix
+    A[0:6,3:6] = U1.T.dot(A[0:6,3:6])
+    A[6:12,3:6] = U2.T.dot(A[6:12,3:6])
+    A[12:18,3:6] = U3.T.dot(A[12:18,3:6])
+    A[18:24,3:6] = U4.T.dot(A[18:24,3:6])
+
+    # Apply QT vertically
+    A0 = np.r_[A[0:3, 3:6], A[6:9, 3:6]]
+    A[0:3, 3:6] = U12.T @ A0
+    A1 = np.r_[A[12:15, 3:6], A[18:21, 3:6]]
+    A[12:15, 3:6] = U34.T @ A1
+
+    A3 = np.r_[A[0:3, 3:6], A[12:15, 3:6]]
+    A[0:3, 3:6] = U1234.T @ A3
+
+    B2 = A[:,3:6]
+    Q2,R2, reduction_tree = ts_qr(B2)
+    Q = np.c_[Q1,Q2]
+    R = np.c_[R1, R2]
+
+    down = np.c_[np.zeros((R2.shape[0], R1.shape[1])), R2]
+    R = np.r_[R, down]
+
+    return Q, R
 
 
+A = np.random.random((4 * 6, 3 * 2))
 
-A = np.random.random((4 * 6, 3 * 3))
-B1 = A[:,0:3]
-Q, R = ts_qr(B1)
-_Q,_R = np.linalg.qr(B1)
-print(np.allclose(_R, R))
-print(np.allclose(Q, _Q))
+tiled_qr(A)
+# B1 = A[:,0:3]
+# Q, R = ts_qr(B1)
+# _Q,_R = np.linalg.qr(B1)
+# print(np.allclose(_R, R))
+# print(np.allclose(Q, _Q))
