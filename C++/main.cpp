4f1ff80921b415be6b5a2eb4f954fed7ab68f39a
flynn <fulin@uw.edu>
2023-04-02 13:49:11 -0800

    add householder function

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,8 +2,15 @@
 #include <Eigen/Dense>
 using namespace Eigen;
 
-MatrixXd householder(MatrixXd A) {
-    return A;
+VectorXd householder(VectorXd u) {
+    float sigma = u.norm();
+    int sign = u[0] >= 0 ? -1 : 1;
+    VectorXd e = ArrayXd::Zero(u.size());
+    e[0] = 1;
+    VectorXd v = sign * sigma * e;
+    u = u - v;
+    VectorXd w = u / u.norm();
+    return w;
 }
 
 void qr_factorization(MatrixXd A, MatrixXd Q, MatrixXd R) {
@@ -11,16 +18,27 @@ void qr_factorization(MatrixXd A, MatrixXd Q, MatrixXd R) {
     int n = A.cols();
 
     for (int i = 0; i < n; i++) {
-        MatrixXd column = A(Eigen::seq(i, n - i), i);
-        MatrixXd w = householder(column);
+        MatrixXd column = A(Eigen::seq(i, n - 1), i);
+        VectorXd w = householder(column);
         MatrixXd I = Eigen::MatrixXd::Identity(m - i, m - i);
-        MatrixXd H_hat = I - 2 * w * w;
+
+        MatrixXd H_hat = I - 2 * w * w.transpose();
+
+        std::cout << H_hat << std::endl;
+        std::cout << H_hat << std::endl;
     }
 }
 
 int main()
 {
-    MatrixXd A = MatrixXd::Random(3, 3);
+    //MatrixXd A = MatrixXd::Random(3, 3);
+    MatrixXd A{
+        {0, 3, 1},
+        {0, 4, -2},
+        {2, 1, 1},
+    };
+    Eigen::HouseholderQR<Eigen::MatrixXd> householderQR(A);
+
     int m = A.rows();
     int n = A.cols();
     MatrixXd Q = Eigen::MatrixXd::Identity(m, m);
